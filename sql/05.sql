bc775b637c77728d11edd89592c621f0e857d451
Oliver Ricken <2oliver.ricken@gmail.com>
2024-03-03 16:01:39 -0800

    finished through 5e

--- sql/05.sql
+++ sql/05.sql
@@ -6,3 +6,16 @@
  * HINT:
  * This can be solved with a self join on the film_actor table.
  */
+
+/*
+ * Who are the actual actors in AMERICAN CIRCUS
+ */
+
+SELECT f2.title
+FROM film f1
+JOIN film_actor fa1 ON (f1.film_id = fa1.film_id)
+JOIN actor ON (fa1.actor_id = actor.actor_id)
+JOIN film_actor fa2 ON (actor.actor_id = fa2.actor_id)
+JOIN film f2 ON (f2.film_id = fa2.film_id)
+WHERE f1.title = 'AMERICAN CIRCUS'
+ORDER BY f2.title;
--- sql/05b.sql
+++ sql/05b.sql
@@ -1,3 +1,17 @@
 /*
  * Write a SQL query that lists the title of all movies where at least 2 actors were also in 'AMERICAN CIRCUS'.
  */
+
+SELECT title
+FROM (
+    SELECT f2.title, count(f2.title)
+    FROM film f1
+    JOIN film_actor fa1 ON (f1.film_id = fa1.film_id)
+    JOIN actor ON (fa1.actor_id = actor.actor_id)
+    JOIN film_actor fa2 ON (actor.actor_id = fa2.actor_id)
+    JOIN film f2 ON (f2.film_id = fa2.film_id)
+    WHERE f1.title = 'AMERICAN CIRCUS'
+    GROUP BY f2.title
+) AS actor_count
+WHERE count > 1
+ORDER BY title;
